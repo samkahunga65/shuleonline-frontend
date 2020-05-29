f5f2a2d4e2e6986f92d0bf57746d987b3acefec1
ohongs <samkahunga65@gmail.com>
2020-05-30 01:00:32 +0300

    s

---
+++ build/precache-manifest.535908e0d87aca4ff695406947d1c4d4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c79cc3f0971c09a1ba76d592c0e3ab97",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21c0e2ab51efe6497cd9",
+    "url": "/static/css/main.4603349d.chunk.css"
+  },
+  {
+    "revision": "58126c36f6209cb0589b",
+    "url": "/static/js/2.d9c6d2c8.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.d9c6d2c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21c0e2ab51efe6497cd9",
+    "url": "/static/js/main.a88854f2.chunk.js"
+  },
+  {
+    "revision": "38e91e65a4b9ec5bf2fd",
+    "url": "/static/js/runtime-main.ad579835.js"
+  }
+]);
